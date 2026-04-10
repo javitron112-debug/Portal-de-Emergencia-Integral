aab21289fcfc99a3a859ce7e7a4d11226e718f73
javitron112-debug <javitron112@gmail.com>
2026-04-10 10:24:28 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Manual Táctico de Emergencias",
+  "short_name": "Emergencias",
+  "start_url": "./INDEX.HTML",
+  "display": "standalone",
+  "background_color": "#1a1a1a",
+  "theme_color": "#d32f2f",
+  "description": "Protocolos offline para escenarios Grid-Down, Mochila 72h y Primeros Auxilios.",
+  "icons": [
+    {
+      "src": "mochila.jpg",
+      "sizes": "192x192",
+      "type": "image/jpeg"
+    },
+    {
+      "src": "mochila.jpg",
+      "sizes": "512x512",
+      "type": "image/jpeg"
+    }
+  ]
+}
